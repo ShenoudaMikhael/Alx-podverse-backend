951a47c841295c6892401898a7a7573543786d8d
Salsabil <salsabeelahmed323@gmail.com>
2024-10-07 20:58:58 +0300

    Adding Update Podcast Route

--- src/controllers/PodcastController.js
+++ src/controllers/PodcastController.js
@@ -41,7 +41,40 @@ class PodcastController {
     }
     }
 
-    
+    static async updatePodcast(req, res) {
+        try {
+            const podcastId = req.params.id;
+            const podcast = await Podcast.findOne({ where: { id: podcastId } });
+
+            if (!podcast) {
+                return res.status(404).json({ msg: 'Podcast not found' });
+            }
+
+            const { title, description, start_date, is_live, cat_id } = req.body;
+
+            const fieldsToUpdate = {};
+            if (title) fieldsToUpdate.title = title;
+            if (description) fieldsToUpdate.description = description;
+            if (start_date) fieldsToUpdate.start_date = start_date;
+            if (is_live) fieldsToUpdate.is_live = is_live;
+            if (cat_id) fieldsToUpdate.cat_id = cat_id;
+
+            if (Object.keys(fieldsToUpdate).length === 0) {
+                return res.status(400).json({ msg: 'No fields to update' });
+            }
+
+            const updatedPodcast = await Podcast.update(
+                fieldsToUpdate,
+                { where: { id: podcastId }}
+            );
+
+            res.json({ msg: 'Podcast updated successfully', podcast: updatedPodcast });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+
 };
 
 module.exports = PodcastController;
--- src/routes/PodcastRoutes.js
+++ src/routes/PodcastRoutes.js
@@ -1,6 +1,8 @@
 const podcastRouter = require('express').Router();
 const PodcastController = require('../controllers/PodcastController');
+const AuthController = require('../controllers/AuthController');
 
-podcastRouter.post('/create', PodcastController.createPodcast);
+podcastRouter.post('/create', AuthController.authMiddleWare, PodcastController.createPodcast);
+podcastRouter.put('/:id', AuthController.authMiddleWare, PodcastController.updatePodcast);
 
 module.exports = podcastRouter;
