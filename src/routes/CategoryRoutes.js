65a381eb8c1cf8ec512ab43a32a2ce60ad08dda1
Salsabil <salsabeelahmed323@gmail.com>
2024-10-13 16:52:36 +0300

    Adding Get Categories Route

--- src/routes/CategoryRoutes.js
+++ src/routes/CategoryRoutes.js
@@ -2,5 +2,6 @@ const categoryRouter = require('express').Router();
 const CategoryController = require('../controllers/CategoryController');
 
 categoryRouter.post('/create', CategoryController.createCategory);
+categoryRouter.get('/get', CategoryController.getCategory);
 
 module.exports = categoryRouter;
